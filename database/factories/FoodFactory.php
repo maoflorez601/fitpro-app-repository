4411bcea563167525167e95f81a4462fe53b9e59
EGSans <juan.ballesteros@uao.edu.co>
2024-05-31 23:25:12 -0500

    pruebas unitarias para las factories

--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -2,22 +2,31 @@
 
 namespace Database\Factories;
 
+use App\Models\Food;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Food>
- */
 class FoodFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Food::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word,
+            'category' => $this->faker->word,
+            'protein' => $this->faker->randomFloat(2, 0, 100),
+            'carbs' => $this->faker->randomFloat(2, 0, 100),
+            'fat' => $this->faker->randomFloat(2, 0, 100),
         ];
     }
 }
